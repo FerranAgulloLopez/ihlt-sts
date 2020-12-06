3eea986a1b9f2a11b264d25f60fd0abdd88fc375
adriantormos <adriantormos98@gmail.com>
2020-12-06 16:37:51 +0100

    Add SVR training
    Now a SVR can be used to perform regression over the test and train sets. Added some extra parameters for this to work, including a global variable named models.

--- auxiliary_code/aggregation_methods.py
+++ auxiliary_code/aggregation_methods.py
@@ -1,10 +1,28 @@
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVR
 
 
-def mean(config, scores):
+models = {
+    'svr': SVR()
+}
+
+
+def mean(config, scores, og_labels, models, test):
     return scores.mean(axis=1)
 
 
-def run_aggregation_method(config, scores):
-    return eval(config['name'])(config, scores)
+def svm(config, scores, og_labels, models, test):
+    mms = MinMaxScaler()
+    if not test:
+        models['svr'] = models['svr'].fit(scores, og_labels)
+    predictions = models['svr'].predict(scores)
+
+    mms = mms.fit(np.reshape(predictions, (len(predictions), 1)))
+    predictions = mms.transform(np.reshape(predictions, (len(predictions), 1)))
+    return np.reshape(predictions, (np.shape(predictions)[0], ))
+
+
+def run_aggregation_method(config, scores, og_labels, test=False):
+    return eval(config['name'])(config, scores, og_labels, models, test)
 
